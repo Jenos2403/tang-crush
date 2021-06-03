6c2c6f3bc4ed4a9057d3886641e020cb1aabb196
Jenos2403 <85280018+Jenos2403@users.noreply.github.com>
2021-06-03 15:05:14 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,17 +1,19 @@
 const CONFIG = {
-    introTitle: 'Bé iu à!',
+    introTitle: 'Babe à!',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
     Đằng sau chữ yêu đây là thương`,
     btnIntro: 'hihi',
-    title: 'Phải chăng anh đã yêu em từ cái nhìn đầu tiên',
+    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thương em <33',
+    btnYes: 'Thương anhhhh nhiềuuu <33',
     btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao anhh lại thương emm hoii <3',
-    btnReply: 'Gửi cho bé <3',
+    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
+    btnReply: 'Gửi cho anh <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà . Yêu em nhiều nhiều 😘😘',
-    messDesc: 'mai mốt gặp anh mua cho hết đồ ăn bé thích nhóoo',
-    btnAccept: 'Okiiiii lun <3'}
+    mess: 'Anh biết mà 🥰. Thương em nhiều nhiều 😘😘',
+    messDesc: 'Nào gặp anh mua nhiều bánh cho bé nhaa.',
+    btnAccept: 'Okiiiii lun <3',
+    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+}
